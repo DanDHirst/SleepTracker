8760a4509f3db5aa2e92068e2b1a9ca6bb1c09af
jhicks5 <joshua.hicks@students.plymouth.ac.uk>
2020-02-21 11:39:33 +0000

    Layout logic for footer

--- SleepTracker/resources/views/layout/footer.blade.php
+++ SleepTracker/resources/views/layout/footer.blade.php
@@ -1,64 +1,63 @@
-@extends('layout.sidebar')
-@section('sidebar')
 
-    <footer class="footer-distributed">
+<footer class="footer-distributed">
 
-        <div class="footer-left">
+    <div class="footer-left">
 
-            <h3>Company<span>logo</span></h3>
+        <h3>Company<span>logo</span></h3>
 
-            <p class="footer-links">
-                <a href="#">Dashboard</a>
-                ·
-                <a href="#">New Entry</a>
-                ·
-                <a href="#">Calendar</a>
-                ·
-                <a href="#">Account</a>
-                ·
-                <a href="#">Advice</a>
-                ·
-                <a href="#">Contact</a>
-            </p>
+        <p class="footer-links">
+            <a href="#">Dashboard</a>
+            ·
+            <a href="#">New Entry</a>
+            ·
+            <a href="#">Calendar</a>
+            ·
+            <a href="#">Account</a>
+            ·
+            <a href="#">Advice</a>
+            ·
+            <a href="#">Contact</a>
+        </p>
 
-            <p class="footer-company-name">Organised Confusion © 2020</p>
+        <p class="footer-company-name">Organised Confusion © 2020</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>64 Zoo Lane</span> London, England</p>
         </div>
 
-        <div class="footer-center">
-
-            <div>
-                <i class="fa fa-map-marker"></i>
-                <p><span>64 Zoo Lane</span> London, England</p>
-            </div>
-
-            <div>
-                <i class="fa fa-phone"></i>
-                <p>0118 999 881 999 119 725 3</p>
-            </div>
-
-            <div>
-                <i class="fa fa-envelope"></i>
-                <p><a href="mailto:support@company.com">support@company.com</a></p>
-            </div>
-
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>0118 999 881 999 119 725 3</p>
         </div>
 
-        <div class="footer-right">
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="mailto:support@company.com">support@company.com</a></p>
+        </div>
 
-            <p class="footer-company-about">
-                <span>About the company</span>
-                Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-            </p>
+    </div>
 
-            <div class="footer-icons">
+    <div class="footer-right">
 
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-github"></i></a>
+        <p class="footer-company-about">
+            <span>About the company</span>
+            Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+        </p>
 
-            </div>
+        <div class="footer-icons">
+
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-linkedin"></i></a>
+            <a href="#"><i class="fa fa-github"></i></a>
 
         </div>
 
-    </footer>
+    </div>
+
+</footer>
+@yield("footer")
