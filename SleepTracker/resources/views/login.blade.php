0a173c00a0ac9dedc5295b6b1a5e63532b405696
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2020-02-28 19:45:42 +0000

    UI update to login page

--- SleepTracker/resources/views/login.blade.php
+++ SleepTracker/resources/views/login.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container" style="padding: 60px" id="content">
     <div class="card-deck">
-        <div class="card bg-dark text-white">
+        <div class="card bg-dark text-white" style="height: fit-content">
             <div class="card-header">
                 <h2>Login</h2>
             </div>
@@ -15,12 +15,12 @@
                     </div>
                     <hr/>
                     <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="text" id="email" placeholder="Email..." minlength="1" name="email" class="form-control" required>
+                        <label for="logUsername">Username:</label>
+                        <input type="text" id="logUsername" placeholder="Username..." minlength="1" name="logUsername" class="form-control" required>
                     </div>
                     <div class="form-group">
-                        <label for="postcode">Postcode:</label>
-                        <input type="text" id="postcode" placeholder="Postcode..." minlength="1" name="postcode" class="form-control" required>
+                        <label for="logPassword">Password:</label>
+                        <input type="password" id="logPassword" placeholder="Password..." minlength="1" name="logPassword" class="form-control" required>
                     </div>
                     <div class="form-group">
                         <button style="" type="submit" name="login" id="login" class="mt-6 btn btn-light">Login</button>
@@ -29,9 +29,9 @@
             </div>
         </div>
 
-        <div class="card bg-dark text-white">
+        <div class="card bg-dark text-white" style="height: fit-content">
             <div class="card-header">
-                <h2>New Customer?</h2>
+                <h2>Register</h2>
             </div>
             <div class="card-body">
                 <form method="post" action="" id="registration" class="login-form">
@@ -40,16 +40,38 @@
                     </div>
                     <hr/>
                     <div class="form-group">
-                        <label for="customer_name">Customer Name:</label>
-                        <input type="text" id="regCustomer_name" placeholder="Customer Name..." minlength="1" name="regCustomer_name" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="postcode">Postcode:</label>
-                        <input type="text" id="regPostcode" placeholder="Postcode..." minlength="1" name="regPostcode" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
+                        <label for="regEmail">Email:</label>
                         <input type="text" id="regEmail" placeholder="Email..." minlength="1" name="regEmail" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="regUsername">Username:</label>
+                        <input type="text" id="regUsername" placeholder="Username..." minlength="1" name="regUsername" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="regPassword">Password:</label>
+                        <input type="password" id="regPassword" placeholder="Password..." minlength="1" name="regPassword" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="regEmail">Email:</label>
+                        <input type="text" id="regEmail" placeholder="Email..." minlength="1" name="regEmail" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="regCountry">Country:</label>
+                        <select id="regCountry" placeholder="Country..." name="regCountry" class="form-control" required>
+                            <option value="Other">Other</option>
+                            <option value="England">England</option>
+                            <option value="Scotland">Scotland</option>
+                            <option value="Wales">Wales</option>
+                            <option value="North Ireland">North Ireland</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="regQuestion">Security Question:</label>
+                        <input type="text" id="regQuestion" placeholder="Question..." name="regQuestion" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="regAnswer">Security Answer:</label>
+                        <input type="text" id="regAnswer" placeholder="Answer..." name="regAnswer" class="form-control" required>
                     </div>
                     <div class="form-group">
                         <button type="submit" name="register" id="register" class="btn btn-light">Register</button>
