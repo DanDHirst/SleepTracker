717105765711bd06e89c745be5e80e3cc1ada11b
Dan <Danhirst123@gmail.com>
2020-03-02 15:06:35 +0000

    added redirect when user is not logged in

--- SleepTracker/resources/views/addSleep.blade.php
+++ SleepTracker/resources/views/addSleep.blade.php
@@ -1,7 +1,11 @@
 @extends('layout.sidebar')
 @extends('layout.footer')
 @section('sidebar')
-
+    @if(!Auth::check())
+        <script>
+            window.location.href = '{{url("home")}}';
+        </script>
+    @endif
     <div class="container" id="content" style="width: 40%">
         <div class="card-deck">
             <div class="card bg-dark text-white">
@@ -11,11 +15,7 @@
                 <div class="card-body">
                     <form method="post" action="addSleep" class="login-form">
                         @csrf
-                        <div class="form-group">
-                            <label for="UserID">UserID:</label>
-                            <input type="text" id="userID" name="userID" class="form-control" value="
-                            @if (Auth::check()){{ Auth::user()->id }}@endif" >
-                        </div>
+
                         <div class="form-group text-center">
                             <p><span>Please input Sleep details below</span></p>
                         </div>
